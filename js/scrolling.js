c525df021b2d3e081da2a4eeab095665271ab730
github-webtest <lord.47.dark@gmail.com>
2021-06-24 23:44:22 +0300

    index

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,15 +1,15 @@
 function next_photo() {
-		document.getElementById("photos_a1").scrollLeft += 150;
+		document.getElementById("photos_a1").scrollLeft += 160;
 }
 
 function back_photo() {
-		document.getElementById("photos_a1").scrollLeft -= 150;
+		document.getElementById("photos_a1").scrollLeft -= 160;
 }
 
 function next_photo_2() {
-		document.getElementById("photos_a2").scrollLeft += 150;
+		document.getElementById("photos_a2").scrollLeft += 160;
 }
 
 function back_photo_2() {
-		document.getElementById("photos_a2").scrollLeft -= 150;
+		document.getElementById("photos_a2").scrollLeft -= 160;
 }

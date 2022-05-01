7f2659a0ff6f237a9687a7d5ec1cdf330ff8b916
github-webtest <lord.47.dark@gmail.com>
2022-05-01 21:37:52 +0300

    index

--- js/scrolling.js
+++ js/scrolling.js
@@ -14,8 +14,4 @@ function back_photo_2() {
 		document.getElementById("photos_a2").scrollLeft -= 160;
 }
 
-function height_100() {
-	document.getElementsByClassName("_3Xz9Z").style.height = "100vh"
-}
-
-height_100();
+document.getElementsByClassName("_3Xz9Z").style.height = "100vh";
